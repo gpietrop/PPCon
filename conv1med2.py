f71c176bac62a295ab21b89e4a4f4b273318da97
GloriaPietropolli <gloria.pietropolli@gmail.com>
2022-12-17 17:31:01 +0100

    debug - now the conv architecture learn from the top to the bottom

--- conv1med2.py
+++ conv1med2.py
@@ -1,6 +1,5 @@
 from torch import nn
 
-dropout_rate = 0.5
 in_channels = 7
 out_channels = 1
 
@@ -9,39 +8,39 @@ class Conv1dMed(nn.Module):
     def __init__(self):
         super(Conv1dMed, self).__init__()
 
-        self.conv1 = nn.Conv1d(in_channels, 64, kernel_size=3, stride=1, padding=2)
+        self.conv1 = nn.Conv1d(in_channels, 64, kernel_size=2, stride=1, padding=2)
         self.bn1 = nn.BatchNorm1d(64)
         self.af1 = nn.SELU()
 
-        self.conv2 = nn.Conv1d(64, 128, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv1d(64, 128, kernel_size=2, stride=2, padding=1)
         self.bn2 = nn.BatchNorm1d(128)
         self.af2 = nn.SELU()
 
-        self.conv3 = nn.Conv1d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv1d(128, 128, kernel_size=4, stride=1, padding=1)
         self.bn3 = nn.BatchNorm1d(128)
         self.af3 = nn.SELU()
 
-        self.conv12 = nn.Conv1d(128, 128, kernel_size=4, stride=1, padding=1)
+        self.conv12 = nn.Conv1d(128, 128, kernel_size=4, stride=1, padding=2)
         self.bn12 = nn.BatchNorm1d(128)
         self.af12 = nn.SELU()
 
-        self.deconv13 = nn.ConvTranspose1d(128, 128, kernel_size=2, stride=2, padding=1)
+        self.deconv13 = nn.ConvTranspose1d(128, 128, kernel_size=2, stride=2, padding=2)
         self.bn13 = nn.BatchNorm1d(128)
         self.af13 = nn.SELU()
 
-        self.conv14 = nn.Conv1d(128, 128, kernel_size=4, stride=1, padding=1)
+        self.conv14 = nn.Conv1d(128, 128, kernel_size=3, stride=1, padding=1)
         self.bn14 = nn.BatchNorm1d(128)
         self.af14 = nn.SELU()
 
-        self.deconv15 = nn.ConvTranspose1d(128, 64, kernel_size=3, stride=2, padding=1)
+        self.deconv15 = nn.ConvTranspose1d(128, 64, kernel_size=2, stride=2, padding=1)
         self.bn15 = nn.BatchNorm1d(64)
         self.af15 = nn.SELU()
 
-        self.conv16 = nn.Conv1d(64, 32, kernel_size=2, stride=2, padding=2)
+        self.conv16 = nn.Conv1d(64, 32, kernel_size=2, stride=2, padding=1)
         self.bn16 = nn.BatchNorm1d(32)
         self.af16 = nn.SELU()
 
-        self.conv17 = nn.Conv1d(32, out_channels, kernel_size=3, stride=1, padding=2)
+        self.conv17 = nn.Conv1d(32, out_channels, kernel_size=3, stride=1, padding=1)
         self.af17 = nn.SELU()
 
     def forward(self, x):
